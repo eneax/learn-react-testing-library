de38030c8d9cf0dc82bf2d829cf684fd3d209cac
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-12-16 15:17:00 +0100

    Add test setup and test case for App component

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    // if you don't have tests that rely on CSS, disable it to speed up test runs
+    css: true,
+  },
+});
