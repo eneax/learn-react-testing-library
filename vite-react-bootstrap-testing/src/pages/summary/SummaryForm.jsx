50930cf0d663e410aa9c43e9ad3750beb3dd314b
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-12-22 15:35:11 +0100

    Update SummaryForm component to pass the tests

--- vite-react-bootstrap-testing/src/pages/summary/SummaryForm.jsx
+++ vite-react-bootstrap-testing/src/pages/summary/SummaryForm.jsx
@@ -1,7 +1,29 @@
+import * as React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 export default function SummaryForm() {
+  const [tcChecked, setTcChecked] = React.useState(false);
+
+  const checkboxLabel = (
+    <span>
+      I agree to <span style={{ color: "blue" }}> Terms and Conditions</span>
+    </span>
+  );
+
   return (
-    <div>
-      <h2>Summary Form</h2>
-    </div>
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
   );
 }
