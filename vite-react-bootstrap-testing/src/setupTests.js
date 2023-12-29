ed53fce5ebafa946ae43952590be276cc432910e
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-12-29 20:47:07 +0100

    Config Mock Service Worker

--- vite-react-bootstrap-testing/src/setupTests.js
+++ vite-react-bootstrap-testing/src/setupTests.js
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom";
+
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./mocks/server";
+
+// Enable API mocking before tests
+beforeAll(() => server.listen());
+
+// Reset any runtime request handlers we may add during the tests
+afterEach(() => server.resetHandlers());
+
+// Clean up once the tests are done
+afterAll(() => server.close());
