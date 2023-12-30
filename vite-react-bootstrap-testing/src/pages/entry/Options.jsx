b64eddfc6811f784485b33106b0b335bfce5e503
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-12-30 14:28:24 +0100

    Add Options and ScoopOption components

---
+++ vite-react-bootstrap-testing/src/pages/entry/Options.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+
+import ScoopOption from "./ScoopOption";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        console.log(`Error fetching ${optionType} data`, error);
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
+
+Options.propTypes = {
+  optionType: PropTypes.string.isRequired, // "scoops" or "toppings"
+};
