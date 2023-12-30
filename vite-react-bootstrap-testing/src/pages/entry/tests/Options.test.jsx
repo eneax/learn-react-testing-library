4e2e71233a30da2324c25ac926cf123452591b47
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-12-30 14:50:54 +0100

    Add toppings tests

--- vite-react-bootstrap-testing/src/pages/entry/tests/Options.test.jsx
+++ vite-react-bootstrap-testing/src/pages/entry/tests/Options.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 
 import Options from "../Options";
 
-test("displays image for each scoop option from server", async () => {
+test("Displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
   // find images coming from server
@@ -13,4 +13,23 @@ test("displays image for each scoop option from server", async () => {
   // confirm alt text of images
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("Displays image for each toppings option from server", async () => {
+  // Mock Service Worker will return three toppings from server
+  render(<Options optionType="toppings" />);
+
+  // find images, expect 3 based on what msw returns
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // check the actual alt text for the images
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
