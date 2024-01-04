f2b3bf7191da392e690bfdca425a78d936500b59
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-04 14:14:27 +0100

    Refactor App component to use  OrderDetailsProvider

--- vite-react-bootstrap-testing/src/App.jsx
+++ vite-react-bootstrap-testing/src/App.jsx
@@ -1,13 +1,17 @@
+import Container from "react-bootstrap/Container";
+
+import { OrderDetailsProvider } from "./contexts/OrderDetails";
+
 import OrderEntry from "./pages/entry/OrderEntry";
 import OrderSummary from "./pages/summary/OrderSummary";
 
 export default function App() {
   return (
-    <main>
-      <h1>Vite + React + Bootstrap</h1>
-
-      <OrderEntry />
-      <OrderSummary />
-    </main>
+    <Container>
+      <OrderDetailsProvider>
+        <OrderEntry />
+        <OrderSummary />
+      </OrderDetailsProvider>
+    </Container>
   );
 }
