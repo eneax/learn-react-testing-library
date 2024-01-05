69aac30d21401ed80cdb935edd225b0b7741a1da
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-05 12:51:37 +0100

    Add order total and order button to OrderEntry component

--- vite-react-bootstrap-testing/src/pages/entry/OrderEntry.jsx
+++ vite-react-bootstrap-testing/src/pages/entry/OrderEntry.jsx
@@ -1,10 +1,32 @@
+import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { formatCurrency } from "../../utilities";
 import Options from "./Options";
 
-export default function OrderEntry() {
+export default function OrderEntry({ setOrderPhase }) {
+  const { totals } = useOrderDetails();
+
   return (
-    <>
+    <div>
+      <h1>Design Your Sundae!</h1>
+
       <Options optionType="scoops" />
       <Options optionType="toppings" />
-    </>
+
+      <h2>Grand total: {formatCurrency(totals.scoops + totals.toppings)}</h2>
+
+      <Button
+        disabled={totals.scoops === 0}
+        onClick={() => setOrderPhase("review")}
+      >
+        Order Sundae!
+      </Button>
+    </div>
   );
 }
+
+OrderEntry.propTypes = {
+  setOrderPhase: PropTypes.func,
+};
