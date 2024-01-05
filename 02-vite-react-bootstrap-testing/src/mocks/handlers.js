8a3e06bd665c3e1d659343cc8b69cd107a564be5
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-05 17:09:58 +0100

    Add server delay and update order phase test

--- 02-vite-react-bootstrap-testing/src/mocks/handlers.js
+++ 02-vite-react-bootstrap-testing/src/mocks/handlers.js
@@ -1,17 +1,22 @@
-import { HttpResponse, http } from "msw";
+import { HttpResponse, delay, http } from "msw";
 
 export const handlers = [
-  http.get("http://localhost:3030/scoops", async () => {
+  http.get("http://localhost:3030/scoops", () => {
     return HttpResponse.json([
       { name: "Chocolate", imagePath: "/images/chocolate.png" },
       { name: "Vanilla", imagePath: "/images/vanilla.png" },
     ]);
   }),
-  http.get("http://localhost:3030/toppings", async () => {
+  http.get("http://localhost:3030/toppings", () => {
     return HttpResponse.json([
       { name: "Cherries", imagePath: "/images/cherries.png" },
       { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
       { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
     ]);
+  }),
+
+  http.post("http://localhost:3030/order", async () => {
+    await delay(100); // simulate server delay (100ms) to show loading spinner
+    return HttpResponse.json({ orderNumber: 123455676 }, { status: 201 });
   }),
 ];
---
+++ 02-vite-react-bootstrap-testing/src/tests/OrderPhase.test.jsx
@@ -0,0 +1,93 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import App from "../App";
+
+test("Order phases for happy path", async () => {
+  const user = userEvent.setup();
+
+  // render App (it already has the provider)
+  const { unmount } = render(<App />);
+
+  // add scoops and toppings
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+
+  const chocolateInput = screen.getByRole("spinbutton", { name: "Chocolate" });
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesCheckbox);
+
+  // find and click order summary button
+  const orderSummaryButton = screen.getByRole("button", {
+    name: /order sundae/i,
+  });
+  await user.click(orderSummaryButton);
+
+  // check summary subtotals
+  const summaryHeading = screen.getByRole("heading", { name: "Order Summary" });
+  expect(summaryHeading).toBeInTheDocument();
+
+  const scoopsHeading = screen.getByRole("heading", { name: "Scoops: $6.00" });
+  expect(scoopsHeading).toBeInTheDocument();
+
+  const toppingsHeading = screen.getByRole("heading", {
+    name: "Toppings: $1.50",
+  });
+  expect(toppingsHeading).toBeInTheDocument();
+
+  // check summary option items
+  const optionItems = screen.getAllByRole("listitem");
+  const optionItemsText = optionItems.map((item) => item.textContent);
+  expect(optionItemsText).toEqual(["1 Vanilla", "2 Chocolate", "Cherries"]);
+
+  // accept terms and conditions
+  const tcCheckbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  await user.click(tcCheckbox);
+
+  // confirm order number on confirmation page
+  const confirmOrderButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+  await user.click(confirmOrderButton);
+
+  // Expect "loading" to show
+  const loading = screen.getByText(/loading/i);
+  expect(loading).toBeInTheDocument();
+
+  // check confirmation page text (this is async)
+  const thankYouHeader = await screen.findByRole("heading", {
+    name: /thank you/i,
+  });
+  expect(thankYouHeader).toBeInTheDocument();
+
+  // expect that loading has disappeared
+  const notLoading = screen.queryByText("loading");
+  expect(notLoading).not.toBeInTheDocument();
+
+  // check that order number is displayed
+  const orderNumber = await screen.findByText(/order number/i);
+  expect(orderNumber).toBeInTheDocument();
+
+  // find and click "new order" button on confirmation page
+  const newOrderButton = screen.getByRole("button", { name: /new order/i });
+  await user.click(newOrderButton);
+
+  // check that scoops and toppings have been reset
+  const scoopsTotal = await screen.findByText("Scoops total: $0.00");
+  expect(scoopsTotal).toBeInTheDocument();
+  const toppingsTotal = screen.getByText("Toppings total: $0.00");
+  expect(toppingsTotal).toBeInTheDocument();
+
+  // explicitly unmount component to trigger network call cancellation on cleanup
+  unmount();
+});
