49b839d219ba4957fbffefb75c7d707f6efc14e7
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-06 10:25:29 +0100

    Add tests for handling errors and disabling order button

--- 02-vite-react-bootstrap-testing/src/pages/entry/tests/OrderEntry.test.jsx
+++ 02-vite-react-bootstrap-testing/src/pages/entry/tests/OrderEntry.test.jsx
@@ -1,10 +1,11 @@
+import userEvent from "@testing-library/user-event";
 import { HttpResponse, http } from "msw";
 
 import { render, screen } from "../../../test-utils/testing-library-utils";
 import { server } from "../../../mocks/server";
 import OrderEntry from "../OrderEntry";
 
-test("handles errors for scoops and toppings routes", async () => {
+test("Handles errors for scoops and toppings routes", async () => {
   server.resetHandlers(
     http.get("http://localhost:3030/scoops", () => {
       return new HttpResponse(null, { status: 500 });
@@ -18,4 +19,26 @@ test("handles errors for scoops and toppings routes", async () => {
 
   const alerts = await screen.findAllByRole("alert");
   expect(alerts).toHaveLength(2);
+});
+
+test("If there are no scoops ordered, disable order button", async () => {
+  const user = userEvent.setup();
+  render(<OrderEntry setOrderPhase={vi.fn()} />);
+
+  // order button should be disabled at first, even before options load
+  const orderButton = screen.getByRole("button", { name: /order sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  // expect button to be enabled after adding scoop
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(orderButton).toBeEnabled();
+
+  // expect button to be disabled again after removing scoop
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
 });
