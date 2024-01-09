ab50f07bc9c3507b7ab41b73ebe29dcb6f789a20
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-09 14:13:51 +0100

    Add tests for rendering user name and email

--- 03-users/src/UserList.test.js
+++ 03-users/src/UserList.test.js
@@ -3,11 +3,12 @@ import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
 test("render one row per user", () => {
-  // render the component
   const users = [
     { name: "John Doe", email: "john.doe@example.com" },
     { name: "Jane Doe", email: "jane.doe@example.com" },
   ];
+
+  // render the component
   render(<UserList users={users} />);
 
   // find all the rows in the table
@@ -15,4 +16,21 @@ test("render one row per user", () => {
 
   // assert that the number of rows is equal to the number of users
   expect(rows).toHaveLength(users.length);
+});
+
+test("render the name and email of each user", () => {
+  const users = [
+    { name: "John Doe", email: "john.doe@example.com" },
+    { name: "Jane Doe", email: "jane.doe@example.com" },
+  ];
+
+  render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
 });
