7ebffe23bbadaa1e2affc09d548187edec414f31
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-09 14:33:28 +0100

    Add test for the App component

--- 03-users/src/App.test.js
+++ 03-users/src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+import App from "./App";
+
+test("it receives a new user and shows it in the list", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByRole("textbox", { name: /name/i });
+  const emailInput = screen.getByRole("textbox", { name: /email/i });
+  const button = screen.getByRole("button");
+
+  await user.click(nameInput);
+  await user.keyboard("John Doe");
+
+  await user.click(emailInput);
+  await user.keyboard("john.doe@example.com");
+
+  await user.click(button);
+
+  const name = screen.getByRole("cell", { name: "John Doe" });
+  const email = screen.getByRole("cell", { name: "john.doe@example.com" });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
