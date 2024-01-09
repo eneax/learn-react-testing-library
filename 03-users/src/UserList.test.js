56cd511b4a0d906d950b2e081bd91cd683321d75
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-09 14:20:32 +0100

    Refactor UserList.test.js to use a renderComponent helper function

--- 03-users/src/UserList.test.js
+++ 03-users/src/UserList.test.js
@@ -2,14 +2,23 @@ import { render, screen, within } from "@testing-library/react";
 
 import UserList from "./UserList";
 
-test("render one row per user", () => {
+// helper function to render the component instead of using beforeEach
+function renderComponent() {
   const users = [
     { name: "John Doe", email: "john.doe@example.com" },
     { name: "Jane Doe", email: "jane.doe@example.com" },
   ];
 
-  // render the component
   render(<UserList users={users} />);
+
+  return {
+    users,
+  };
+}
+
+test("render one row per user", () => {
+  // render the component
+  const { users } = renderComponent();
 
   // find all the rows in the table
   const rows = within(screen.getByTestId("users")).getAllByRole("row");
@@ -19,12 +28,7 @@ test("render one row per user", () => {
 });
 
 test("render the name and email of each user", () => {
-  const users = [
-    { name: "John Doe", email: "john.doe@example.com" },
-    { name: "Jane Doe", email: "jane.doe@example.com" },
-  ];
-
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   for (let user of users) {
     const name = screen.getByRole("cell", { name: user.name });
