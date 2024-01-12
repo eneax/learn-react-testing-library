026f360e84c584f3e31ac77060996a571c998a1f
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-12 16:10:20 +0100

    Refactor RepositoriesSummary.test.js to display information about the repository

--- 08-codesplain/src/components/repositories/RepositoriesSummary.test.js
+++ 08-codesplain/src/components/repositories/RepositoriesSummary.test.js
@@ -2,17 +2,20 @@ import { screen, render } from "@testing-library/react";
 
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("displays the primary language of the repository summary", () => {
+test("displays information about the repository", () => {
   const repository = {
-    stargazers_count: 1,
-    open_issues: 1,
+    stargazers_count: 5,
+    open_issues: 30,
     forks: 1,
     language: "JavaScript",
   };
 
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText("JavaScript");
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(`${value}`));
 
-  expect(language).toBeInTheDocument();
+    expect(element).toBeInTheDocument();
+  }
 });
