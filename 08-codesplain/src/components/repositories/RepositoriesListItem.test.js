384ac0edf4cb0a87de41f875e538ade7de45a344
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-14 07:32:02 +0100

    Add RepositoriesListItem test with Router context

---
+++ 08-codesplain/src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,25 @@
+import { screen, render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+
+import RepositoriesListItem from "./RepositoriesListItem";
+
+function renderComponent() {
+  const repository = {
+    full_name: "facebook/react",
+    language: "JavaScript",
+    description: "A JS library for building UIs",
+    owner: "facebook",
+    name: "react",
+    html_url: "https://github.com/facebook/react",
+  };
+
+  return render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repository} />
+    </MemoryRouter>
+  );
+}
+
+test("displays link to the GitHub homepage for this repo", () => {
+  renderComponent();
+});
