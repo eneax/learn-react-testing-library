6d097296eee6c4681a3114a7a668968c229f1e03
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-01-15 17:08:25 +0100

    Fix SWR cache issue and update AuthButtons test

--- 08-codesplain/src/components/auth/AuthButtons.test.js
+++ 08-codesplain/src/components/auth/AuthButtons.test.js
@@ -1,14 +1,18 @@
 import { screen, render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import { createServer } from "../../test/server";
+import { SWRConfig } from "swr";
 
+import { createServer } from "../../test/server";
 import AuthButtons from "./AuthButtons";
 
 async function renderComponent() {
   render(
-    <MemoryRouter>
-      <AuthButtons />
-    </MemoryRouter>
+    // Fix swr cache issue
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <MemoryRouter>
+        <AuthButtons />
+      </MemoryRouter>
+    </SWRConfig>
   );
 
   await screen.findAllByRole("link");
@@ -40,5 +44,36 @@ describe("when not signed in", () => {
 
     const signOutButton = screen.queryByRole("link", { name: /sign out/i });
     expect(signOutButton).not.toBeInTheDocument();
+  });
+});
+
+describe("when signed in", () => {
+  // createServer() => GET /api/user => { user: { id: 1, email: 'testuser@test.com' } }
+  createServer([
+    {
+      path: "/api/user",
+      res: () => ({
+        user: { id: 1, email: "testuser@test.com" },
+      }),
+    },
+  ]);
+
+  test("sign in and sign up are not visible", async () => {
+    await renderComponent();
+
+    const signInButton = screen.queryByRole("link", { name: /sign in/i });
+    expect(signInButton).not.toBeInTheDocument();
+
+    const signUpButton = screen.queryByRole("link", { name: /sign up/i });
+    expect(signUpButton).not.toBeInTheDocument();
+  });
+
+  test("sign out is visible", async () => {
+    await renderComponent();
+
+    const signOutButton = screen.getByRole("link", { name: /sign out/i });
+
+    expect(signOutButton).toBeInTheDocument();
+    expect(signOutButton).toHaveAttribute("href", "/signout");
   });
 });
